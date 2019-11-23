685cda3b3a467e3be87d32aa6dbc6c3171b49d9e
naotaro0123 <n.ikewada@gmail.com>
2019-11-24 00:56:22 +0900

    add useCannon Sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import './style.css';
 import { Canvas } from 'react-three-fiber';
-// import HelloWorld from './HelloWorld';
+import { PCFSoftShadowMap } from 'three';
+import HelloWorld1 from './HelloWorld';
 import HelloWorld2 from './HelloWorld2';
+import HellowCannonBox from './HelloCannonBox';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Canvas style={{ width: '500px', height: '500px' }}>
+      {/* <Canvas style={{ width: '500px', height: '500px' , background: '#000000'}}>
+        <HelloWorld1 />
         <HelloWorld2 />
+      </Canvas> */}
+      <Canvas
+          camera={{ position: [0, 0, 15] }}
+          onCreated={( props ) => {
+            props.gl.shadowMap.enabled = true;
+            props.gl.shadowMap.type = PCFSoftShadowMap;
+          }}
+        >
+        <HellowCannonBox />
       </Canvas>
     </div>
   );
